639a3a17bd0fd223673c89e47e28ce49fcbfe7bc
ivyxjc <ivyxjc1994@hotmail.com>
2019-09-22 20:15:46 +0800

    :sparkles: add aq test

---
+++ libra-core/src/test/kotlin/xyz/ivyxjc/libra/ProtobufTest.kt
@@ -0,0 +1,44 @@
+package xyz.ivyxjc.libra
+
+import org.apache.commons.lang3.RandomStringUtils
+import xyz.ivyxjc.libra.core.models.RawTransaction
+import xyz.ivyxjc.libra.core.models.UsecaseTxn
+import xyz.ivyxjc.libra.core.models.protoModels.ProtoRawTransaction
+import xyz.ivyxjc.libra.core.models.protoModels.ProtoUsecaseTxn
+import kotlin.random.Random
+
+
+fun buildUcBytes(): ByteArray {
+    val ucTxn = UsecaseTxn()
+    ucTxn.gcGuid = "asdfasdf"
+    ucTxn.sourceId = Random.nextLong()
+    ucTxn.ucTxnId = Random.nextLong()
+    ucTxn.attributes.putIfAbsent(Random.nextLong().toString(), Random.nextLong().toString())
+    ucTxn.attributes.putIfAbsent(Random.nextLong().toString(), Random.nextLong().toString())
+    ucTxn.attributes.putIfAbsent(Random.nextLong().toString(), Random.nextLong().toString())
+    ucTxn.attributes.putIfAbsent(Random.nextLong().toString(), Random.nextLong().toString())
+
+    val builder = ProtoUsecaseTxn.PUsecaseTxn.newBuilder()
+    builder.gcGuid = ucTxn.gcGuid
+    builder.sourceId = ucTxn.sourceId
+    builder.ucTxnId = ucTxn.ucTxnId
+    builder.putAllAttributes(ucTxn.attributes)
+    val data = builder.build()
+    val bytes = data.toByteArray()
+    return bytes
+}
+
+fun buildRawTransBytes(): ByteArray {
+    val rawTrans = RawTransaction()
+    rawTrans.gcGuid = Random.nextLong().toString()
+    rawTrans.sourceId = Random.nextLong()
+    rawTrans.rawRecord = RandomStringUtils.random(100)
+    println(rawTrans)
+    val builder = ProtoRawTransaction.PRawTransaction.newBuilder()
+    builder.gcGuid = rawTrans.gcGuid
+    builder.sourceId = rawTrans.sourceId
+    builder.rawRecord = rawTrans.rawRecord
+    val data = builder.build()
+    val bytes = data.toByteArray()
+    return bytes
+}
