7b69c63f551753082728efae4281b2e494818c4e
ivyxjc <ivyxjc1994@hotmail.com>
2019-09-23 23:45:34 +0800

    :sparkles: add active mq endpoint listener

--- libra-core/src/main/kotlin/xyz/ivyxjc/libra/core/connection/JmsConnectionUtils.kt
+++ libra-core/src/main/kotlin/xyz/ivyxjc/libra/core/connection/JmsConnectionUtils.kt
@@ -1,13 +1,19 @@
 package xyz.ivyxjc.libra.core.connection
 
+import org.apache.activemq.ActiveMQConnectionFactory
 import org.apache.activemq.artemis.jms.client.ActiveMQQueueConnectionFactory
 import javax.jms.QueueConnectionFactory
 
 class JmsConnectionUtils {
     companion object {
         @JvmStatic
-        fun aqConnectionFactory(url: String): QueueConnectionFactory {
+        fun artemisConnectionFactory(url: String): QueueConnectionFactory {
             return ActiveMQQueueConnectionFactory(url)
+        }
+
+        @JvmStatic
+        fun aqConnectionFactory(url: String): QueueConnectionFactory {
+            return ActiveMQConnectionFactory(url)
         }
     }
 
--- libra-core/src/main/kotlin/xyz/ivyxjc/libra/core/platforms/Platform.kt
+++ libra-core/src/main/kotlin/xyz/ivyxjc/libra/core/platforms/Platform.kt
@@ -1,7 +1,34 @@
 package xyz.ivyxjc.libra.core.platforms
 
+import xyz.ivyxjc.libra.core.loggerFor
 import xyz.ivyxjc.libra.core.models.AbstractTransaction
+import xyz.ivyxjc.libra.core.models.RawTransaction
+import xyz.ivyxjc.libra.core.models.UsecaseTxn
 
 interface Dispatcher<T : AbstractTransaction> {
     fun dispatch(trans: T)
+}
+
+class BlankRawTransDispatcher : Dispatcher<RawTransaction> {
+    companion object {
+        @JvmStatic
+        private val log = loggerFor(BlankRawTransDispatcher::class.java)
+    }
+
+    override fun dispatch(trans: RawTransaction) {
+        log.debug("get rawTrans: {}", trans)
+    }
+
+}
+
+class BlankUcTxnDispatcher : Dispatcher<UsecaseTxn> {
+    companion object {
+        @JvmStatic
+        private val log = loggerFor(BlankUcTxnDispatcher::class.java)
+    }
+
+    override fun dispatch(trans: UsecaseTxn) {
+        log.debug("get ucTxn: {}", trans)
+    }
+
 }
