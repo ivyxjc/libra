9280702924f255032b12ac16e3fa9754e1443f36
ivyxjc <ivyxjc1994@hotmail.com>
2019-10-09 00:42:30 +0800

    :bug: fix UT and RawTransMapper

--- libra-core/src/test/kotlin/xyz/ivyxjc/libra/core/dao/GcTransMapperTest.kt
+++ libra-core/src/test/kotlin/xyz/ivyxjc/libra/core/dao/GcTransMapperTest.kt
@@ -1,20 +1,23 @@
-package xyz.ivyxjc.libra.dao
+package xyz.ivyxjc.libra.core.dao
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mybatis.spring.boot.test.autoconfigure.AutoConfigureMybatis
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 import xyz.ivyxjc.libra.buildGcTxn
-import xyz.ivyxjc.libra.core.dao.GcTransMapper
+import xyz.ivyxjc.libra.core.TestStartRunner
 
-@RunWith(SpringRunner::class)
 @SpringBootTest
+@RunWith(SpringRunner::class)
+@AutoConfigureMybatis
+@ContextConfiguration(classes = [TestStartRunner::class])
 open class GcTransMapperTest {
 
     @Autowired
     private lateinit var gcTransMapper: GcTransMapper
-
 
     @Test
     fun testInsert() {
--- libra-core/src/test/kotlin/xyz/ivyxjc/libra/core/dao/RawTransMapperTest.kt
+++ libra-core/src/test/kotlin/xyz/ivyxjc/libra/core/dao/RawTransMapperTest.kt
@@ -1,21 +1,23 @@
-package xyz.ivyxjc.libra.dao
+package xyz.ivyxjc.libra.core.dao
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mybatis.spring.boot.test.autoconfigure.AutoConfigureMybatis
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 import xyz.ivyxjc.libra.buildRawTrans
-import xyz.ivyxjc.libra.core.dao.RawTransMapper
-import xyz.ivyxjc.libra.core.models.RawTransaction
+import xyz.ivyxjc.libra.core.TestStartRunner
 
-@RunWith(SpringRunner::class)
 @SpringBootTest
+@RunWith(SpringRunner::class)
+@AutoConfigureMybatis
+@ContextConfiguration(classes = [TestStartRunner::class])
 open class RawTransMapperTest {
 
     @Autowired
     private lateinit var mRawTransMapper: RawTransMapper
-
 
     @Test
     fun testInsert() {
