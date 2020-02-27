324465dc9d62871b0cacaf940156486f269886ad
ivyxjc <ivyxjc1994@hotmail.com>
2020-02-27 21:55:03 +0800

    :sparkles: libra jms support

---
+++ libra-jms/src/main/kotlin/xyz/ivyxjc/libra/jms/model/inner/LibraJmsListenerYaml.kt
@@ -0,0 +1,15 @@
+package xyz.ivyxjc.libra.jms.model.inner
+
+internal class LibraJmsListenerYaml {
+    lateinit var destination: String
+    lateinit var messageListener: String
+    lateinit var dispatcher: String
+
+    var sourceIds: String = "ALL"
+    var id: String = ""
+    var containerFactory: String = ""
+    var subscription: String = ""
+    var selector: String = ""
+    var concurrency: String = ""
+
+}
